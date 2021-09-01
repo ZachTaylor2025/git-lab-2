0095f7eaa4f721427d0777dd2f307acacdbe7c1a
Zach <zt133220@ohio.edu>
2021-08-31 20:07:41 -0400

    Initial commit

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Zach Taylor
+ *        Date: 8/31/2021
+ * Description: git Lab 2
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!" << endl;
+    return 0;
+}// main
